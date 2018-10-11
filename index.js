08a39b5050b2a8e84189bddb015240e2b7e24c3f
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-11 18:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function johnLennonFacts(facts) {
     sameFactsButMoreExciting.push(facts[factCounter] + "!!!");
     factCounter++;
   }
-  console.log(sameFactsButMoreExciting);
+  return(sameFactsButMoreExciting);
 }
 
 johnLennonFacts(facts);

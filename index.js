fe8de91a3625238a0ee7c1cf745e2341558e11c2
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-11 17:51:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
 
+var instruments = ['rhythm guitar', 'bass guitar', 'lead guitar', 'the drums'];
+
+function theBeatlesPlay(musicians, instruments) {
+  
+}

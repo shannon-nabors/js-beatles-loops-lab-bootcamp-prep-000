728d960db1f6091bd9026e380c8cc9db95ead174
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-11 17:58:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var musiciansPlayingInstruments = [];
 
 function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i++) {
-    console.log(musicians.i + " plays " + instruments.i)
+    console.log(musicians[i] + " plays " + instruments[i])
   }
 }

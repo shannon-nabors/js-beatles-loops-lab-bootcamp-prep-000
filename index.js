2ae9dc4ec90b51965138725a0f1509b706033ddc
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-11 18:36:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 const facts = [
-  "His middle name was Winston",
+  /*"His middle name was Winston",
   "His dad was the mascot for his school soccer team",
-  "He was introduced to LSD when a dentist spiked his coffee at a dinner party"
+  "He was introduced to LSD when a dentist spiked his coffee at a dinner party"*/
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
   ];
   
 var sameFactsButMoreExciting = [];

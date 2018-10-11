2f63a570879eb36dcafd08e151f5d0b48fa9c25d
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-11 18:28:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,5 @@ function iLoveTheBeatles(loveCounter) {
     beatlesFanaticism.push("I love the Beatles!");
     loveCounter++;
   } while (loveCounter < 15);
-  console.log(beatlesFanaticism);
+  return(beatlesFanaticism);
 }
-
-iLoveTheBeatles(loveCounter);
